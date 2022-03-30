84c72cb4862e6278ac18ca2e548c7a165aded5c1
johnstat101 <johnjorogek@gmail.com>
2022-03-30 17:41:27 +0300

    add save delete functionality to models

--- recipeapp/models.py
+++ recipeapp/models.py
@@ -1,12 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
-    profile_pic = models.ImageField(upload_to='images',null=True)
+    name = models.CharField(blank=True, max_length=120)
+    profile_pic = models.ImageField(upload_to='images/', default='default.png')
     bio = models.CharField(max_length=120)
     
     def __str__(self):
          return str(self.user)
+    
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
 
 class Recipe(models.Model):
     recipe_name = models.CharField(max_length=120)
@@ -19,9 +27,14 @@ class Recipe(models.Model):
     created_by = models.ForeignKey(Profile, on_delete=models.CASCADE)
     publication_date = models.DateTimeField('Date Published')
 
-
     def __str__(self):
         return self.recipe_name
+    
+    def save_recipe(self):
+        self.save()
+
+    def delete_recipe(self):
+        self.delete()
 
 
 class Ingredient(models.Model):
@@ -29,3 +42,6 @@ class Ingredient(models.Model):
     description = models.CharField(max_length=200)
     quantity = models.CharField(max_length=50)
     unit = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
