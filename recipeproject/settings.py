848d7032e28d3c877051d070d4d12a9b5c9966dc
Esther-Anyona <jkemuntoe@gmail.com>
2022-03-30 21:35:45 +0300

    Add environment variables

--- recipeproject/settings.py
+++ recipeproject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config, Csv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4k7udlsqpd6i5a=hw+7#x&w#-xbp+i8sja4vyyig1l0isr0i0('
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'recipeapp.apps.RecipeappConfig',
 ]
 
 MIDDLEWARE = [
