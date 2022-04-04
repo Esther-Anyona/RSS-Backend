7fc9405a61cb05e42c6ee63853eaca7488114a87
johnstat101 <johnjorogek@gmail.com>
2022-04-04 20:06:43 +0300

    edit ratings

--- recipeapp/models.py
+++ recipeapp/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    user = models.OneToOneField(User,on_delete=models.CASCADE, related_name='profile')
     name = models.CharField(blank=True, max_length=120)
+    email = models.EmailField(max_length=100, blank=True)
     profile_pic = models.ImageField(upload_to='images/', default='default.png')
     bio = models.CharField(max_length=120)
 
@@ -17,20 +18,28 @@ class Profile(models.Model):
         self.delete()
 
 class Rating(models.Model):
-    user = models.ForeignKey(Profile,on_delete=models.CASCADE)
-    rating = models.CharField(max_length=5)
+    choices = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5')
+        )
+    user_rating = models.PositiveBigIntegerField(choices=choices, default=0)
+    status = models.CharField(max_length=10, blank=True)
+
 
 class Recipe(models.Model):
     recipe_name = models.CharField(max_length=100)
     ingredient = models.TextField()
     category = models.CharField(max_length=120)
-    recipe_pic = models.ImageField(upload_to="images",blank = True)
+    recipe_pic = models.ImageField(upload_to="images",default='default.png')
     country = models.CharField(max_length=50)
     procedure = models.TextField('Instructions')
     guests_served = models.CharField(max_length=50)
-    created_by = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True, blank=True)
-    rating = models.ForeignKey(Rating, on_delete=models.CASCADE)
+    recipe_rating = models.ForeignKey(Rating, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.recipe_name
