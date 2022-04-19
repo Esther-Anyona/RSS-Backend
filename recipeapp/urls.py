c58f40ea7b487d9d1fde3f20a08b38e03df4ed6a
johnstat101 <johnjorogek@gmail.com>
2022-04-19 13:05:58 +0300

    edit views

--- recipeapp/urls.py
+++ recipeapp/urls.py
@@ -8,11 +8,6 @@ router.register('ratings', views.RatingViewSet, basename='ratings')
 urlpatterns = [
     path('api/recipes', views.recipe_list),
     re_path(r'^api/recipes/(?P<pk>[0-9]+)$', views.recipe_detail),
-    # path('api/recipes', views.index),
-    path('api/recipes/create', views.create_recipe),
-    # re_path(r'^api/recipes/(?P<pk>[0-9]+)$', views.get_recipe),
-    re_path(r'^api/recipes/(?P<pk>[0-9]+)/delete$', views.delete_recipe),
     path('api/recipes/search', views.search_recipes),
-    re_path(r'^api/recipes/(?P<pk>[0-9]+)/update$', views.update_recipe),
     re_path(r'^api/recipes/(?P<pk>[0-9]+)/ratings/$', views.RatingViewSet.as_view({'get': 'list', 'post': 'create'}),name='recipe_ratings'),
 ]
--- recipeapp/views.py
+++ recipeapp/views.py
@@ -52,27 +52,6 @@ def recipe_list(request):
         return Response({'message': '{} Tutorials were deleted successfully!'.format(count[0])}, status=status.HTTP_204_NO_CONTENT)
 
 
-
-@api_view(['GET'])
-def index(request):
-    # retrieve all recipes
-    if request.method == 'GET':
-        recipes = Recipe.objects.all()
-        recipes_serializer = RecipeSerializer(recipes, many=True)
-        return Response(recipes_serializer.data)
-
-
-@api_view(['POST'])
-def create_recipe(request):
-    # create and save new recipe 
-    if request.method == 'POST':
-        recipes_serializer = RecipeSerializer(data=request.data)
-        if recipes_serializer.is_valid():
-            recipes_serializer.save()
-            return Response(recipes_serializer.data, status=status.HTTP_201_CREATED)
-        return Response(recipes_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
 @api_view(['GET'])
 def search_recipes(request):
     # retrieve objects by title
@@ -85,49 +64,6 @@ def search_recipes(request):
 
         recipes_serializer = RecipeSerializer(recipes, many=True)
         return Response(recipes_serializer.data)
-
-
-@api_view(['GET'])
-def get_recipe(request, pk):
-    # view single recipe
-    try: 
-        recipe = Recipe.objects.get(pk=pk) 
-    except Recipe.DoesNotExist: 
-        return Response({'message': 'The Recipe does not exist'}, status=status.HTTP_404_NOT_FOUND) 
- 
-    if request.method == 'GET': 
-        recipe_serializer = RecipeSerializer(recipe)
-        return Response(recipe_serializer.data)
-
-
-@api_view(['PUT'])
-def update_recipe(request, pk):
-    # retrieve single recipe
-    try: 
-        recipe = Recipe.objects.get(pk=pk) 
-    except Recipe.DoesNotExist: 
-        return Response({'message': 'The Recipe does not exist'}, status=status.HTTP_404_NOT_FOUND) 
- 
-    if request.method == 'PUT':
-        recipe_serializer = RecipeSerializer(recipe, request.data)
-        if recipe_serializer.is_valid(): 
-            recipe_serializer.save() 
-            return Response(recipe_serializer.data) 
-        return Response(recipe_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['DELETE'])
-def delete_recipe(request, pk):
-    # delete single recipe
-    try: 
-        recipe = Recipe.objects.get(pk=pk)
-    except Recipe.DoesNotExist: 
-        return Response({'message': 'The Recipe does not exist'}, status=status.HTTP_404_NOT_FOUND) 
- 
-    if request.method == 'DELETE':
-        recipe.delete() 
-        return Response({'message': 'Recipe was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
-
 
 class RatingViewSet(viewsets.ModelViewSet):
     # permission_classes = (IsAuthenticated,)
